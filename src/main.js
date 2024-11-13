a577d2cb89dc3412b62f1294c586448435f5431b
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-11-13 14:41:00 +0000

    add crypto.randomUUID()

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 import "./style.css";
+import { formEl, inputEl, yearEl } from "./domSelection";
 
-const Years = document.querySelector(".year");
-const formElement = document.querySelector("form");
-// IIFE - Immediately Invoked Function Expression
-(function () {
-  const date = newDate.Date().getFullYear();
-  Years.textContent = `${date}`;
-})();
+const tasks = [];
 
-formElement.addEventListener("submit", (e) => {
-  // Prevent the default form submit
-  e.preventDefault();
-  console.log("Form submitted");
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault(); // Prevent the page from reloading
+
+  //  Checking for empty input
+  if (!inputEl.value) {
+    return;
+  }
+
+  tasks.push({
+    title: inputEl.value,
+    isCompleted: false,
+    id: crypto.randomUUID(),
+  });
+
+  console.log(tasks);
+
+  //  Empty the input field
+  inputEl.value = "";
 });
+
+// IIFE
+(function () {
+  const year = new Date().getFullYear();
+
+  // MARK: Update the DOM
+  yearEl.textContent = `${year}`;
+})();
