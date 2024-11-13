61c225773e5073be7f8d351ca430bea1a92e7434
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-11-13 13:43:10 +0000

    Add function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import "./style.css";
+
+const Years = document.querySelector(".year");
+const formElement = document.querySelector("form");
+// IIFE - Immediately Invoked Function Expression
+(function () {
+  const date = newDate.Date().getFullYear();
+  Years.textContent = `${date}`;
+})();
+
+formElement.addEventListener("submit", (e) => {
+  // Prevent the default form submit
+  e.preventDefault();
+  console.log("Form submitted");
+});
